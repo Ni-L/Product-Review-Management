cc945d2082579e673cf9271992c835404459a1ad
unknown <nilimawadal1@gmail.com>
2021-05-24 20:06:45 +0530

    [Nilima]Added UC7 Creted Table

--- Product-Review-Managment/Product-Review-Managment/Program.cs
+++ Product-Review-Managment/Product-Review-Managment/Program.cs
@@ -59,18 +59,10 @@ namespace Product_Review_Managment
             //RetrivedTop3RecordsFromList(productReviewList);
             // CountingProductId(productReviewList);
             //RetrivedProductIdAndReview(productReviewList);
-            SkiipingRecord(productReviewList);
-        }
-        public static void CreateDataTble()//Adding Method For query
-        {
-            DataTable table = new DataTable();
-            table.Columns.Add("ProductID");
-            table.Columns.Add("Production");
-
-            table.Rows.Add("1", "Laptop");
-            table.Rows.Add("2", "Mobile");
-            table.Rows.Add("3", "TV");
-
+            //  SkiipingRecord(productReviewList);
+            AddDataTable();
+            Console.ReadLine() ;
+            
         }
         //Adding Display Method 
         //Datatable Represents one table in memory data
@@ -192,6 +184,44 @@ namespace Product_Review_Managment
                 Console.WriteLine(ex.Message);
             }
         }
+        public static void AddDataTable()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("productId");
+            table.Columns.Add("UserId");
+            table.Columns.Add("Ratings");
+            table.Columns.Add("Reviews");
+            table.Columns.Add("isLike");
+            table.Rows.Add("1", "1", "5", "Good", true);
+            table.Rows.Add("2", "3", "4", "Good", true);
+            table.Rows.Add("3", "4", "4", "Good", true);
+            table.Rows.Add("4", "5", "5", "Good", true);
+            table.Rows.Add("5", "4", "3", "Average", true);
+            table.Rows.Add("6", "5", "1", "Bad", false);
+            table.Rows.Add("7", "10", "5", "Good", true);
+            table.Rows.Add("8", "10", "5", "Good", true);
+            table.Rows.Add("9", "3", "4", "Good", true);
+            table.Rows.Add("10", "2", "2", "Bad", false);
+            table.Rows.Add("11", "3", "3", "Average", true);
+            table.Rows.Add("12", "1", "3", "Average", false);
+        }
+        public static void DisplayTable(DataTable table) //Create Display method
+        {
+            try
+            {
+                var tabledata = from product in table.AsEnumerable() select product.Field<string>("ProductName"); 
+                foreach (var item in tabledata) 
+                {
+                    Console.WriteLine($"ProductName:- " +item);
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
     }
 
 }
